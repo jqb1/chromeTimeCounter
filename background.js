bc920193ba4822781d08e16f9662d5cc5ead5d09
Jakub Kozioł <jakub.koziol12@gmail.com>
2018-09-03 12:17:31 +0200

    printing current domain

--- background.js
+++ background.js
@@ -1,21 +1,23 @@
 console.log("backgroud  running!");
 
+
 chrome.browserAction.onClicked.addListener(buttonClicked);
 
+//check when tab is changed
+chrome.tabs.onActiveChanged.addListener(tabChanged);
 
-var params={
+let params={
     'active': true,
     'lastFocusedWindow': true
 };
 
 function printPage() {
+    //get currently opened tab info
     chrome.tabs.query(params, function (tabs) {
-        console.log('we\'re here');
-        var url = tabs[0].url;
-        console.log(url);
+         url = tabs[0].url;
+         console.log(url);
     });
 }
-
 
 function buttonClicked(tab) {
     let msg={
@@ -24,4 +26,48 @@ function buttonClicked(tab) {
 
     printPage();
     chrome.tabs.sendMessage(tab.id,msg);
+}
+
+function tabChanged(){
+    console.log("tab changed!");
+    //when tab is changed, get tab url
+    chrome.tabs.query(params, function (tabs) {
+        //get url
+        url = tabs[0].url;
+
+        checkUrl(url);
+
+    });
+
+}
+
+function checkUrl (url) {
+
+
+    let website = truncateUrl(url)
+    console.log(website);
+
+}
+
+function truncateUrl(url) {
+    let counter = 0;
+    let output_word='';
+
+    //save only domain name
+    for (letter of url){
+
+        if (letter =='/'){
+            counter++;
+        }
+        if (counter>=2 && counter<3){
+            if (letter=='/')
+                continue;
+
+            output_word=output_word.concat(letter);
+        }
+    }
+
+    return output_word;
+
+
 }
